5abf3cc77b5e71bf2aa8ce5150d50373fe35ba26
Iulian Olaru <iulianolaru249@yahoo.com>
2021-01-22 10:36:34 +0200

    lab 10 apd + lab 10 RL

---
+++ APD/tema2/untitled/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+class Clasa {
+    int atribut;
+
+    int operator. () {
+        return 5;
+    };
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
